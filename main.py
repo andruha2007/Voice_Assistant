8f73a3fc52052f8f2a54b62c638f2ca9082fc407
Андрей Уваров <141756989+andruha2007@users.noreply.github.com>
2025-08-05 16:09:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
-import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
+import speech_recognition as sr  # распознавание пользовательской речи (Speech-To-Text)
 import wave  # создание и чтение аудиофайлов формата wav
 import json  # работа с json-файлами и json-строками
 import os  # работа с файловой системой
+import socket
+
+commands = {
+    "greeting": ["привет", "здравствуй", "добрый день", "доброе утро", "добрый вечер"],
+    'searching': ["найти", "найди"],
+    "opening": ["открой", "запусти"],
+    "writing": ["записывай", "конcпектируй"],
+    "waiting": ["слушаю вас", ""],
+    "special_commands": ["за работу"],
+    "graduation": []
+
+}
 
 def start(recognizer, microphone):
     recognized_data = ""
@@ -10,28 +22,32 @@ def start(recognizer, microphone):
 
     while waiting:
         with microphone as sourse:
+            recognizer.adjust_for_ambient_noise(sourse, duration=1)
             print("Ожидаю вызова...")
             audio = recognizer.listen(sourse, phrase_time_limit=None)
+
             with open("wait_for_call.wav", "wb") as file:
                 file.write(audio.get_wav_data())
 
             try:
                 print("Перевожу...")
                 recognized_data = recognizer.recognize_google(audio, language="RU-ru").lower()
-            except speech_recognition.UnknownValueError:
+            except sr.UnknownValueError:
                 pass
             print(recognized_data)
-            if "привет" in recognized_data:
+            if "джин" in recognized_data:
                 waiting = False
             recognized_data=""
 
     os.remove("wait_for_call.wav")
     return listen_for_command(recognizer,microphone)
 
+
 def listen_for_command(recognizer, microphone):
 
     with microphone as sourse:
         recognized_data = ""
+        recognizer.adjust_for_ambient_noise(sourse, duration=1)
 
         try:
             print("Ожидаю команду...")
@@ -40,7 +56,7 @@ def listen_for_command(recognizer, microphone):
             with open("wait_for_command.wav", "wb") as file:
                 file.write(audio.get_wav_data())
 
-        except speech_recognition.WaitTimeoutError:
+        except sr.WaitTimeoutError:
             print("Can you check if your microphone is on, please?")
             return
 
@@ -48,26 +64,46 @@ def listen_for_command(recognizer, microphone):
         try:
             print("Перевожу...")
             recognized_data = recognizer.recognize_google(audio, language="RU-ru").lower()
-        except speech_recognition.UnknownValueError:
+
+            execute_command_with_name(recognized_data)
+
+        except sr.UnknownValueError:
             pass
 
-        # в случае проблем с доступом в Интернет происходит попытка
-        # использовать offline-распознавание через Vosk
-        except speech_recognition.RequestError:
+        except sr.RequestError:
             print("Trying to use offline recognition...")
         os.remove("wait_for_command.wav")
-        return recognized_data
+        return execute_command_with_name(recognized_data)
 
+
+def execute_command_with_name(command_name):
+    print(command_name)
+    for key in commands.keys():
+        if command_name in commands[key]:
+            return key
+    else:
+        print("Команда не распознана")
+
+def f():
+    return "Соединение разорвано"
 
 if __name__ == "__main__":
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("www.example.com", 80))
+        print("Connected successfully!")
 
-    # инициализация инструментов распознавания и ввода речи
-    recognizer = speech_recognition.Recognizer()
-    microphone = speech_recognition.Microphone()
+        recognizer = sr.Recognizer()
+        microphone = sr.Microphone()
 
-    while True:
-        # старт записи речи с последующим выводом распознанной речи
-        # и удалением записанного в микрофон аудио
-        voice_input = start(recognizer, microphone)
-        print(voice_input)
+        while True:
+            voice_input = start(recognizer, microphone)
+
+            print(voice_input)
+    except socket.error as e:
+        print(f"Connection failed: {e}")
+
+    finally:
+        sock.close()
+
 
